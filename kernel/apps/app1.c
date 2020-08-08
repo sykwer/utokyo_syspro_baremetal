86edd398f9ffebb718b27aae442d5d20bf385cb8
sykwer <sykwer@gmail.com>
2020-08-09 07:28:36 +0900

    Use absolute addressing jmp for MMU implementation assignment

--- kernel/apps/app1.c
+++ kernel/apps/app1.c
@@ -1,11 +1,13 @@
+unsigned long long text_section_top = 0x104000000;
+
 void app1() {
-  while (1) {
-    // char *str = "Hello from app1\r\n";
+  // char *str = "Hello from app1\r\n";
 
-    // TODO: Say hello from here by issuing `puts` syscall
-    // syscall_puts(str);
+  // TODO: Say hello from here by issuing `puts` syscall
+  // syscall_puts(str);
 
-    volatile int i = 100000000;
-    while (i--);
-  }
+  volatile int i = 100000000;
+  while (i--);
+
+  asm volatile ("jmp *%0" :: "m"(text_section_top));
 }
--- kernel/apps/app3.c
+++ kernel/apps/app3.c
@@ -1,11 +1,13 @@
+unsigned long long text_section_top = 0x106000000;
+
 void app3() {
-  while (1) {
-    // char *str = "Hello from app3\r\n";
+  // char *str = "Hello from app3\r\n";
 
-    // TODO: Say hello from here by issuing `puts` syscall
-    // syscall_puts(str);
+  // TODO: Say hello from here by issuing `puts` syscall
+  // syscall_puts(str);
 
-    volatile int i = 100000000;
-    while (i--);
-  }
+  volatile int i = 100000000;
+  while (i--);
+
+  asm volatile ("jmp *%0" :: "m"(text_section_top));
 }
