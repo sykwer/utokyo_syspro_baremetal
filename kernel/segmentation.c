8ce39676d54fe212e18c0ab6d58f6fe69b9ac6de
Takahiro Ishikawa <sykwer@gmail.com>
2023-04-24 11:44:34 +0900

    fix

--- kernel/segmentation.c
+++ kernel/segmentation.c
@@ -6,16 +6,24 @@ const unsigned long long GDT[] = {
   0x00cf93000000ffff, /* base=0, limit=4GB, mode=data(rw-),kernel */
 };
 
+/*
 struct GDTR {
   unsigned short limit;
   unsigned long long base;
-} __attribute((packed));
+} __attribute__((packed));
+*/
 
 void load_gdt(unsigned long long addr);
 
 void init_segmentation() {
+  /*
   struct GDTR gdtr;
   gdtr.limit = sizeof(GDT);
   gdtr.base = (unsigned long long)GDT;
+  load_gdt((unsigned long long)&gdtr);
+  */
+  unsigned long long gdtr[2];
+  gdtr[0] = ((unsigned long long)GDT << 16) | (sizeof(GDT) - 1);
+  gdtr[1] = ((unsigned long long)GDT >> 48);
   load_gdt((unsigned long long)&gdtr);
 }
